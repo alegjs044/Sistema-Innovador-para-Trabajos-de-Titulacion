d9584c4025ef26a13f127bf602f6b7e48f23fa97
alegjs044 <alegjs044@gmail.com>
2024-05-25 13:38:06 -0600

    add

---
+++ Backend/server.js
@@ -0,0 +1,41 @@
+// biblioteca-backend/server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/biblioteca', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const UserSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+const User = mongoose.model('User', UserSchema);
+
+app.post('/register', async (req, res) => {
+  const { email, password } = req.body;
+  const user = new User({ email, password });
+  await user.save();
+  res.send(user);
+});
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && user.password === password) {
+    res.send(user);
+  } else {
+    res.status(400).send('Invalid credentials');
+  }
+});
+
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
+});
